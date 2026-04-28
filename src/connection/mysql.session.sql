8c15b5e5dc0ddd93ebc6fc2c5fb8d5ec86d8f68f
Erick-008 <erick.s0usa@outlook.com>
2026-04-28 16:21:19 -0300

    primeiro commit

---
+++ src/connection/mysql.session.sql
@@ -0,0 +1,4 @@
+INSERT INTO `crud_sql`.`usuarios` (`nome`, `email`, `senha`) VALUES ('erick', 'erick@email.com', '123456');
+
+show tables;
+
---
+++ src/model/usuarioModel.java
@@ -0,0 +1,63 @@
+package model;
+import java.time.LocalDateTime;
+
+public class usuarioModel {
+
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean status;
+
+
+    public usuarioModel() {
+     nome = "";
+     email = "";
+     senha = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public boolean isStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
